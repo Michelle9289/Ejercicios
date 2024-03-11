23bdfeeb51a1f194734be0c71b858b68d0e3b573
Michelle Limón <michellecl9289@gmail.com>
2024-03-10 21:57:00 -0600

    se agrega ejercicio 2

---
+++ Ejercicio2.js
@@ -0,0 +1,7 @@
+var number = [1, 2, 4, 5];
+var doublesNumber = number.map(function (x) {
+    let result= x * 2;
+  return x * 2;
+});
+console.log("The firts numbers are", number)
+console.log("The output should be", doublesNumber)
